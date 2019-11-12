a96a54e998850a67071aed9a17a6dc160baf9fd2
Anastusha <anastasiatea@mail.ru>
2019-11-12 21:21:33 +0300

    i.tests & user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get  'static_pages/contact'
+  #get 'static_pages/home'
+  #get 'static_pages/help'
+  #get 'static_pages/about'
+  #get  'static_pages/contact'
+  root             'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 end
