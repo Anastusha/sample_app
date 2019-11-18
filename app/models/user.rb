a13e800d2271cab54b959902399906af3b00cd28
Anastusha <anastasiatea@mail.ru>
2019-11-18 22:41:28 +0300

    test for error_message

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     #before_save { self.email = email.downcase }
     before_save { email.downcase! }
 
-    validates :name, presence: true
     validates :email, presence: true
 
     validates :name,  presence: true, length: { maximum: 50 }
