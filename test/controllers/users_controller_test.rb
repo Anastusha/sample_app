ab3f68dfcb7a8223eae1961974e0499a513e3463
Anastusha <anastasiatea@mail.ru>
2019-12-10 14:13:56 +0300

    delete-users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -50,5 +50,18 @@ test "should redirect edit when not logged in" do
     assert_redirected_to root_url
   end
 
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, params: {id: @user}
+    end
+    assert_redirected_to login_url
+  end
 
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, params: {id: @user}
+    end
+    assert_redirected_to root_url
+  end
 end
