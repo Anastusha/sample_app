3457c115af173028e9c98d92d4fa22b43a729104
Anastusha <anastasiatea@mail.ru>
2019-12-16 18:13:43 +0300

    Add account activations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,10 +25,25 @@ class UsersController < ApplicationController
   #     render 'new'
   #   end
   # end
+
+
+
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     @user.send_activation_email
+  #     UserMailer.account_activation(@user).deliver_now
+  #     flash[:info] = "Please check your email to activate your account."
+  #     redirect_to root_url
+  #   else
+  #     render 'new'
+  #   end
+  # end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,16 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  # Активирует аккаунт.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Отправляет электронное письмо для активации.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
   private
 
