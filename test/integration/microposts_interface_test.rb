24cc7929000eb433da4245b6de02523b49033895
Anastusha <anastasiatea@mail.ru>
2019-12-27 14:17:47 +0300

    еще не работает

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -12,13 +12,13 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
     # Невалидная отправка формы.
     assert_no_difference 'Micropost.count' do
-      post microposts_path, micropost: { content: "" }
+      post microposts_path,params: {micropost: { content: "" }}
     end
     assert_select 'div#error_explanation'
     # Валидная отправка формы.
     content = "This micropost really ties the room together"
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path,params: { micropost: { content: content }}
     end
     assert_redirected_to root_url
     follow_redirect!
